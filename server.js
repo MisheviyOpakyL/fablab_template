4527d2b8f663848eeec36357bb2174944bf09958
MisheviyOpakyL <MisheviyOpakyL@yandex.ru>
2020-02-11 13:30:17 +0300

    zatychka

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = 80;
+
+app.use(express.json({ extended: true }));
+
+app.use("/", express.static(path.join(__dirname, "client")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log("app running on port " + PORT)
+});
